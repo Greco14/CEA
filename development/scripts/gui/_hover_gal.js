8e6c247285755e2505753ac10ad37a0889ece14b
Greco <greco.dav.14@gmail.com>
2014-11-19 20:40:46 -0600

    upx

--- development/scripts/gui/_hover_gal.js
+++ development/scripts/gui/_hover_gal.js
@@ -12,10 +12,12 @@ Hover.prototype.bind= function(){
 		s = self.settings;
 	
 	s.pics_hovers.hover(function(){
-		var id= $(this).attr('id');
-		$('#'+id).stop().animate({opacity:1}, 250, 'easeInOutQuad');
+		
+		var id= $(this).attr('id');ç
+		console.log('#'+id+'-hover');
+		$('#'+id+'-hover').stop().animate({opacity:1}, 250, 'easeInOutQuad');
 	},function(){
 		var id= $(this).attr('id');
-		$('#'+id).stop().animate({opacity:0}, 250, 'easeInOutQuad');
+		$('#'+id+'-hover').stop().animate({opacity:0}, 250, 'easeInOutQuad');
 	});
 };
